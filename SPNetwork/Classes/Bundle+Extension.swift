0c7c489d8ec90e72e0e63c75d07170d53f51fa37
sarvesh567 <sp395862@gmail.com>
2021-11-17 17:24:36 +0530

    add bundle.swift

---
+++ SPNetwork/Classes/Bundle+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Bundle.swift
+//  SPNetwork
+//
+//  Created by Amit Sharma on 17/11/21.
+//
+
+import Foundation
+extension Bundle{
+    var getBundleIdentifier: String?{
+        return bundleIdentifier
+    }
+    var getAppVersion: String?{
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildNumber: String?{
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var appName: String?{
+        return infoDictionary?["CFBundleName"] as? String
+    }
+}
